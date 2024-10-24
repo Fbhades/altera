6d0be034bec36e2539ca3818896aaa1f5eb78dae
Mohamed Hedi Bejaoui <bejaouimedhedi2@gmail.com>
2024-10-24 20:38:39 +0100

    c

--- app/flightpage/[id]/page.tsx
+++ app/flightpage/[id]/page.tsx
@@ -56,24 +56,82 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-gray-100">
       <main className="container mx-auto p-6">
-        <div className="bg-white p-8 rounded-lg shadow-md">
+      <div className="bg-white p-8 rounded-lg shadow-md">
           <h2 className="text-4xl font-bold mb-6 text-gray-900">Flight Details: {flightDetails.destination}</h2>
           <div className="mb-8">
             <h3 className="text-2xl font-semibold mb-4 text-gray-700">Flight Information</h3>
-            <p className="text-gray-700">Destination: {flightDetails.destination}</p>
-            <p className="text-gray-700">Departure: {flightDetails.depart}</p>
-            <p className="text-gray-700">Airline: {flightDetails.airline}</p>
-            <p className="text-gray-700">Date: {new Date(flightDetails.date).toLocaleString()}</p>
-            <p className="text-gray-700">Available Seats: {flightClass === 'economy' ? (flightDetails as Flight_details_economy).economy_available_seats : (flightDetails as Flight_details_business).business_available_seats}</p>
-            <p className="text-gray-700">Price: ${basePrice.toFixed(2)}</p>
-            <p className="text-gray-700">Baggage Capacity: {flightClass === 'economy' ? (flightDetails as Flight_details_economy).economy_baggage_capacity : (flightDetails as Flight_details_business).business_baggage_allowance}</p>
-            {flightClass === 'economy' && (
-              <p className="text-gray-700">Extra Baggage Cost: ${(flightDetails as Flight_details_economy).economy_extra_baggage_cost}</p>
-            )}
-            {flightClass === 'business' && (
-              <p className="text-gray-700">Lounge Access: {(flightDetails as Flight_details_business).business_lounge_access ? 'Yes' : 'No'}</p>
-            )}
-          </div>
+            <div className="mb-4">
+              <p className="text-gray-700">Destination: {flightDetails.destination}</p>
+              <p className="text-gray-700">Departure: {flightDetails.depart}</p>
+              <p className="text-gray-700">Airline: {flightDetails.airline}</p>
+              <p className="text-gray-700">Date: {new Date(flightDetails.date).toLocaleString()}</p>
+            </div>
+            <div className="mb-4">
+              <h4 className="text-xl font-semibold mb-2 text-gray-700">Class</h4>
+              <div className="grid grid-cols-2 gap-4">
+                <button 
+                  className={`py-2 px-4 rounded-lg font-semibold col-span-1 ${flightClass === 'economy' ? 'bg-yellow-400 text-gray-900' : 'bg-gray-200 text-gray-700'}`}
+                  onClick={() => setFlightClass('economy')}
+                >
+                  Economy
+                </button>
+                <button 
+                  className={`py-2 px-4 rounded-lg font-semibold col-span-1 ${flightClass === 'business' ? 'bg-yellow-400 text-gray-900' : 'bg-gray-200 text-gray-700'}`}
+                  onClick={() => setFlightClass('business')}
+                >
+                  Business
+                </button>
+              </div>
+            </div>
+            {/* <div className="mb-4">
+              <h4 className="text-xl font-semibold mb-2 text-gray-700">Seat</h4>
+              <div className="grid grid-cols-3 gap-4">
+                <button 
+                  className={`py-2 px-4 rounded-lg font-semibold col-span-1 ${selectedSeat === 'window' ? 'bg-yellow-400 text-gray-900' : 'bg-gray-200 text-gray-700'}`}
+                  onClick={() => setSelectedSeat('window')}
+                >
+                  Window
+                </button>
+                <button 
+                  className={`py-2 px-4 rounded-lg font-semibold col-span-1 ${selectedSeat === 'aisle' ? 'bg-yellow-400 text-gray-900' : 'bg-gray-200 text-gray-700'}`}
+                  onClick={() => setSelectedSeat('aisle')}
+                >
+                  Aisle
+                </button>
+                <button 
+                  className={`py-2 px-4 rounded-lg font-semibold col-span-1 ${selectedSeat === 'middle' ? 'bg-yellow-400 text-gray-900' : 'bg-gray-200 text-gray-700'}`}
+                  onClick={() => setSelectedSeat('middle')}
+                >
+                  Middle
+                </button>
+              </div> */}
+            {/* </div> */}
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-4 mb-4">
+              <div>
+                <h4 className="text-xl font-semibold mb-2 text-gray-700">Available Seats</h4>
+                <p className="text-gray-700">{flightClass === 'economy' ? (flightDetails as Flight_details_economy).economy_available_seats : (flightDetails as Flight_details_business).business_available_seats}</p>
+              </div>
+              <div>
+                <h4 className="text-xl font-semibold mb-2 text-gray-700">Price</h4>
+                <p className="text-gray-700">{flightClass === 'economy' ? (flightDetails as Flight_details_economy).economy_price : (flightDetails as Flight_details_business).business_price}</p>
+              </div>
+              <div>
+                <h4 className="text-xl font-semibold mb-2 text-gray-700">Baggage Capacity</h4>
+                <p className="text-gray-700">{flightClass === 'economy' ? (flightDetails as Flight_details_economy).economy_baggage_capacity : (flightDetails as Flight_details_business).business_baggage_allowance}</p>
+              </div>
+              {flightClass === 'economy' && (
+                <div>
+                  <h4 className="text-xl font-semibold mb-2 text-gray-700">Extra Baggage Cost</h4>
+                  <p className="text-gray-700">{(flightDetails as Flight_details_economy).economy_extra_baggage_cost}</p>
+                </div>
+              )}
+              {flightClass === 'business' && (
+                <div>
+                  <h4 className="text-xl font-semibold mb-2 text-gray-700">Lounge Access</h4>
+                  <p className="text-gray-700">{(flightDetails as Flight_details_business).business_lounge_access ? 'Yes' : 'No'}</p>
+                </div>
+              )}
+            </div>
 
           {/* Meals Section */}
           <div className="mb-8">
@@ -92,26 +150,7 @@ export default function Home() {
               ))}
             </div>
           </div>
-
-          {/* Select Flight Class */}
-          <div className="mb-8">
-            <h3 className="text-2xl font-semibold mb-4 text-gray-700">Select Flight Class</h3>
-            <div className="flex space-x-4">
-              <button 
-                className={`py-2 px-4 rounded-lg font-semibold ${flightClass === 'economy' ? 'bg-yellow-400 text-gray-900' : 'bg-gray-200 text-gray-700'}`}
-                onClick={() => setFlightClass('economy')}
-              >
-                Economy
-              </button>
-              <button 
-                className={`py-2 px-4 rounded-lg font-semibold ${flightClass === 'business' ? 'bg-yellow-400 text-gray-900' : 'bg-gray-200 text-gray-700'}`}
-                onClick={() => setFlightClass('business')}
-              >
-                Business
-              </button>
-            </div>
           </div>
-
           {/* Total Cost */}
           <div className="mb-8">
             <h3 className="text-xl font-semibold mb-4 text-gray-700">Total Cost: ${totalCost.toFixed(2)}</h3>
